1865d830a10ff0aa3bc4aa64c246e1bf18a09ca0
Ferosnow95 <alizafar9361@gmail.com>
2019-08-02 19:13:06 -0400

    Final Edits

---
+++ upcCalculator/src/main/java/MemontoCache/PreviousCalculation.java
@@ -0,0 +1,11 @@
+package MemontoCache;
+
+public interface PreviousCalculation {
+    double getFirstNumber();
+    void setFirstNumber(double firstNumber);
+    double getSecondNumber();
+    void setSecondNumber(double secondNumber);
+    double getResult();
+    void setResult(double result);
+
+}
